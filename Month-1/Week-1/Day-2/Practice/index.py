69f91a78432fb6cc949adc2134412296278f47b6
Jitendra Mishra <jitendramishra223355@gmail.com>
2026-06-11 17:31:33 +0530

    loops

---
+++ Month-1/Week-1/Day-2/Practice/index.py
@@ -0,0 +1,16 @@
+# name = "jitu"
+# age = 25
+# height = 5.8
+# is_valid = True
+
+
+# name = input("Enter your full name:")
+
+# print("Your name is:", name)
+
+
+# addition
+x = int(input("enter a number: "));
+y = int(input("enter another number: "));
+result = x // y
+print("The flow division of", x, "and", y, "is:", result)
---
+++ Month-1/Week-1/Day-3/Practice/index.py
@@ -0,0 +1,55 @@
+# if (True):
+#     print("This is a false statement")
+
+
+# a = -5 ;
+# if(a > 0):
+#     print("a is greater than 0");
+# else:
+#     print("a is less than 0");
+
+
+# If-elif-else statement
+
+# battery = int(input("Enter the battery percentage: "))
+# if battery == 100:
+#     print("Battery is full")
+# elif battery > 80:
+#     print("Battery is good")
+# elif battery > 50:
+#     print("Battery is low")
+# else:
+#      print("Battery is critical")
+
+
+    #  nested if-else statement
+
+# email = input("Enter your email: ")
+# password = input("Enter your password: ")
+
+
+# if email == "xyz@email.com":
+#     if password == "1234":
+#         print("Login successful")
+#     else:
+#         print("Incorrect password")
+
+# for i in range(4):
+#     print(i)
+
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(1, 25, 5):
+#     print(i)
+
+# while loop
+
+# a = 1
+# while a <= 5:
+#     print(a)
+#     a += 1
+
+for x in range(5, -1, -1):
+   
+    print(x)
